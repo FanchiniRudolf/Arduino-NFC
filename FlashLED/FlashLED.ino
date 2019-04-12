fbdc8c71e893c0ba4a62d7f7f9a96c5f68577925
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-04-11 23:46:05 -0500

    Update FlashLED.ino

--- FlashLED/FlashLED.ino
+++ FlashLED/FlashLED.ino
@@ -1,16 +1,22 @@
+#define buttonPin 5
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pinMode(2,OUTPUT);
-  pinMode(4,OUTPUT);
-  digitalWrite(2,HIGH);
-  digitalWrite(4,LOW);
-  delay(1500);
-  pinMode(2, INPUT);
-  pinMode(4,INPUT);
+  int status = digitalRead(buttonPin);
+  if(status == HIGH){
+    pinMode(2,OUTPUT);
+    pinMode(4,OUTPUT);
+    digitalWrite(2,HIGH);
+    digitalWrite(4,LOW);
+  }
+  else{
+    pinMode(2, INPUT);
+    pinMode(4,INPUT);
+  }
   delay(1500);
 }

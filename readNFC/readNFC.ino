9b8793d44a0c81687a0c8055c01c22a0c0b3467d
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-04-12 08:05:12 -0500

    Update readNFC.ino

--- readNFC/readNFC.ino
+++ readNFC/readNFC.ino
@@ -1,9 +1,35 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println("Aproach card");
 
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  MFRC522::StatusCode status;
+  
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+
+  }
+  Serial.println("**Card Detected:**");
+  
 }
